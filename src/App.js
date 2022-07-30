e09ac0d914920332f6c9394cc5a1244787d995d2
Brian James Celine <brianceline@gmail.com>
2022-07-31 00:34:58 +0100

    Props and Data Edit

--- src/App.js
+++ src/App.js
@@ -3,27 +3,33 @@ import './App.css';
 import FurnitureProductTable from './Components/FurnitureProductTable';
 
 const PRODUCTS = [
-  {category: "Dining Table 6'", price: "£2950.00", stocked: true, name: "Directoire"},
-  {category: "Dining Table 7'", price: "£3150.00", stocked: true, name: "Directoire"},
-  {category: "Dining Table 8'", price: "£3450.00", stocked: false, name: "Directoire"},
-  {category: "Dining Table 6'", price: "£2950.00", stocked: true, name: "Rochembeau"},
-  {category: "Dining Table 7'", price: "£3150.00", stocked: true, name: "Rochembeau"},
-  {category: "Dining Table 8'", price: "£3450.00", stocked: false, name: "Rochembeau"},
-  {category: "Dining Table 6'", price: "£2950.00", stocked: true, name: "Heritage"},
-  {category: "Dining Table 7'", price: "£3150.00", stocked: true, name: "Heritage"},
-  {category: "Dining Table 8'", price: "£3450.00", stocked: false, name: "Heritage"},
-  {category: "Dining Chair", price: "£525.00", stocked: true, name: "Directoire"},
-  {category: "Dining Armchair", price: "£600.00", stocked: false, name: "Directoire"},
-  {category: "Dining Chair", price: "£550.00", stocked: true, name: "Rochembeau"},
-  {category: "Dining Armchair", price: "£650.00", stocked: false, name: "Rochembeau"},
-  {category: "Dining Chair", price: "£575.00", stocked: true, name: "Heritage"},
-  {category: "Dining Armchair", price: "£675.00", stocked: false, name: "Heritage"},
+  {category: "Table 6'", price: "£2950.00", stocked: true, name: "Directoire Cherrywood"},
+  {category: "Table 7'", price: "£3150.00", stocked: true, name: "Directoire Cherrywood"},
+  {category: "Table 8'", price: "£3450.00", stocked: false, name: "Directoire Cherrywood"},
+  {category: "Table 6'", price: "£2950.00", stocked: true, name: "Rochembeau Walnut/Oak"},
+  {category: "Table 7'", price: "£3150.00", stocked: true, name: "Rochembeau Walnut/Oak"},
+  {category: "Table 8'", price: "£3450.00", stocked: false, name: "Rochembeau Walnut/Oak"},
+  {category: "Table 6'", price: "£2950.00", stocked: true, name: "Heritage Cherrywood"},
+  {category: "Table 7'", price: "£3150.00", stocked: true, name: "Heritage Cherrywood"},
+  {category: "Table 8'", price: "£3450.00", stocked: false, name: "Heritage Cherrywood"},
+  {category: "Chair", price: "£525.00", stocked: true, name: "Directoire"},
+  {category: "Chair", price: "£550.00", stocked: true, name: "Rochembeau"},
+  {category: "Chair", price: "£575.00", stocked: true, name: "Heritage"},
+  {category: "Armchair", price: "£600.00", stocked: false, name: "Directoire"},
+  {category: "Armchair", price: "£650.00", stocked: false, name: "Rochembeau"},
+  {category: "Armchair", price: "£675.00", stocked: false, name: "Heritage"},
+  {category: "Sideboard", price: "£4995.00", stocked: false, name: "Rochembeau"},
+  {category: "Sideboard", price: "£3995.00", stocked: true, name: "Heritage"},
+  {category: "Sideboard", price: "£4500.00", stocked: false, name: "Directoire"},
+  {category: "Display Cabinet", price: "£2995.00", stocked: false, name: "Rochembeau"},
+  {category: "Display Cabinet", price: "£1995.00", stocked: true, name: "Heritage"},
+  {category: "Display Cabinet", price: "£2500.00", stocked: false, name: "Directoire"},
 
 ];
 
 function App() {
   return (
-    <FurnitureProductTable />
+    <FurnitureProductTable products={PRODUCTS} />
   );
 }
 
--- src/Components/ProductTable.js
+++ src/Components/ProductTable.js
@@ -2,7 +2,24 @@ import React from 'react'
 import ProductCategoryRow from './ProductCategoryRow'
 import ProductRow from './ProductRow'
 
-export default function ProductTable() {
+export default function ProductTable(props) {
+  const { products } = props;
+
+  const rows = [];
+  let lastCategory = null;
+
+  products.forEach((product) => {
+    if (product.category !== lastCategory) {
+      rows.push(
+        <ProductCategoryRow />
+      )
+    }
+    rows.push(
+      <ProductRow />
+    )
+    lastCategory = product.category;
+  })
+
   return (
     <table>
       <thead>
